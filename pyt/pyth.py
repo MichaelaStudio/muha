de6a068e9ddac03c45e9956fe03768e3b1372b1d
girummasresha736 <girummasresha736@gmail.com>
2025-10-23 11:19:41 +0300

    pop

---
+++ pyt/pyth.py
@@ -0,0 +1,18 @@
+
+
+# calculator 
+def addithion(x,y):
+    return x+y
+def subtraction(x,y):
+    return x-y
+def multiplication(x,y):
+    return x*y
+def division(x,y):
+    return x/y
+x=20
+y=10
+
+print(addithion(x,y))
+print(subtraction(x,y))
+print(multiplication(x,y))
+print(division(x,y))
---
+++ python/30-Days-Of-Python/tt.py
@@ -0,0 +1,7 @@
+name = michaela
+age = 20
+gpa = 4.0
+is student = true 
+print(type(name))
+print(type(age))
+print(type(gpa))
